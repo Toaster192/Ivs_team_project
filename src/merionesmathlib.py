33fcf86ee489b33e2e70ef91d38b6f00aa5a4a68
Ivana Saranová <xsaran02@stud.fit.vutbr.cz>
2018-03-26 16:20:41 +0200

    Added: File documentation, Add function, Add function documentation

---
+++ src/merionesmathlib.py
@@ -0,0 +1,14 @@
+## @package merioneslib
+#  Merioneslib is a mathematical library for Meriones calculator.
+#
+#  More details...
+
+##
+# @brief Sum of two numbers
+#
+# @param a First number to be added
+# @param b Second number to be added
+#
+# @return Sum of two numbers a and b
+def Add(a,b):
+    return a + b
