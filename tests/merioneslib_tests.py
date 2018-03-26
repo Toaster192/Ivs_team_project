d98716d255cf5ec8dc57a47b51cd5f3a3c6b2a56
Jaromír Wysoglad <xwysog00@stud.fit.vutbr.cz>
2018-03-26 14:24:53 +0200

    Added tests for adding decimal numbers

--- tests/merioneslib_tests.py
+++ tests/merioneslib_tests.py
@@ -29,3 +29,8 @@ class MerioneslibTestAdd(unittest.testcase):
         self.assertEqual(self.math.add(5, -7), -2)
         self.assertEqual(self.math.add(-7, 8), 1)
         self.assertEqual(self.math.add(-7, 7), 0)
+
+    def test_add_decimal(self):
+        self.assertEqual(self.math.add(2.25, 4.5), 6.75)
+        self.assertEqual(self.math.add(-2.25, 4.5), 2.25)
+        self.assertEqual(self.math.add(-2.25, -4.5), -6.75)
