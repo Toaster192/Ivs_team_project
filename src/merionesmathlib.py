1d5e592213a55e1fa1fe83381420a4b47db2d0eb
Ivana Saranová <xsaran02@stud.fit.vutbr.cz>
2018-04-04 21:50:25 +0200

    Added imported methods from mandatory and extra functions libs

--- src/merionesmathlib.py
+++ src/merionesmathlib.py
@@ -6,6 +6,9 @@
 #  such as sum, difference, multiplication or division, advanced mathematical
 #  operations such as power, ..., logarithm,.... **DOPLNIT**
 
+from src.extraFunctions import *
+from src.mandatoryFunctions import *
+
 ##
 # @brief Class for Meriones Math Library
 class MerionesLib:
