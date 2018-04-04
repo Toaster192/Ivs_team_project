0823ff48e8e6075fd7bc7225ede30b07a774d32d
Ivana Saranová <xsaran02@stud.fit.vutbr.cz>
2018-04-04 22:18:07 +0200

    Corrected indentations in meriones library

--- src/merionesmathlib.py
+++ src/merionesmathlib.py
@@ -89,111 +89,111 @@ def ParetheParse(str_input, remains):
 '''
 
 
-def factorial(self, n):
-    """
-    Method computes factorial of n
+    def factorial(self, n):
+        """
+        Method computes factorial of n
 
-    @param n: number of which factorial is calculated
-    @return: factorial of number n
-    """
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-
-def power(self, base, exponent):
-    """
-    Method calculates a base raised to a power
-
-    @param base: base of the power
-    @param exponent: exponent determines how many times the base will be multiplied
-    @return: base raised to the power
-    """
-    return base**exponent
+        @param n: number of which factorial is calculated
+        @return: factorial of number n
+        """
+        if n == 0:
+            return 1
+        else:
+            return n * factorial(n-1)
 
 
-def root(self, n):
-    """
-    Method calculates square root of n
+    def power(self, base, exponent):
+        """
+        Method calculates a base raised to a power
 
-    @param n: number of which square root will be calculate
-    @return: square root of n
-    """
-    return n ** 0.5
-
-def ln(self, x):
-    """
-    Method calculates natural logarithm of x
-
-    @param x: number of which natural logarithm will be calculate
-    @return: natural logarithm of x
-    """
-    n = 100000000.0
-    return n * ((x ** (1/n)) - 1)
+        @param base: base of the power
+        @param exponent: exponent determines how many times the base will be multiplied
+        @return: base raised to the power
+        """
+        return base**exponent
 
 
-def convert_weight(self, x, units):
-    """
-    Method converts number x to units given by parameter units
+    def root(self, n):
+        """
+        Method calculates square root of n
 
-    @param x: number of kilograms
-    @param units: units that user requested
-    @return: number x converted to given units
-    """
-    if units == 'mg':
-        return x * 1000 ** 2
-    elif units == 'g':
-        return x * 1000
-    elif units == 'lb':
-        return x * 2.20
-    elif units == 't':
-        return x * 0.001
+        @param n: number of which square root will be calculate
+        @return: square root of n
+        """
+        return n ** 0.5
 
+    def ln(self, x):
+        """
+        Method calculates natural logarithm of x
 
-def convert_length(self, x, units):
-    """
-    Method converts number x to units given by parameter units
-
-    @param x: number of kilometers
-    @param units: units that user requested
-    @return: number x converted to given units
-    """
-    if units == 'm':
-        return x * 1000
-    elif units == 'cm':
-        return x * 100000
-    elif units == 'mm':
-        return x * 1000**2
-    elif units == 'mi':
-        return x * 0.62137199
+        @param x: number of which natural logarithm will be calculate
+        @return: natural logarithm of x
+        """
+        n = 100000000.0
+        return n * ((x ** (1/n)) - 1)
 
 
-def convert_time(self, x, units):
-    """
-    Method converts number x to units given by parameter units
+    def convert_weight(self, x, units):
+        """
+        Method converts number x to units given by parameter units
 
-    @param x: number of hours
-    @param units: units that user requested
-    @return: number x converted to given units
-    """
-    if units == 'min':
-        return x * 60
-    elif units == 's':
-        return x * 3600
-    elif units == 'day':
-        return x * 0.0416667
+        @param x: number of kilograms
+        @param units: units that user requested
+        @return: number x converted to given units
+        """
+        if units == 'mg':
+            return x * 1000 ** 2
+        elif units == 'g':
+            return x * 1000
+        elif units == 'lb':
+            return x * 2.20
+        elif units == 't':
+            return x * 0.001
 
 
-def convert_degrees(self, x, units):
-    """
-    Method converts number x to units given by parameter units
+    def convert_length(self, x, units):
+        """
+        Method converts number x to units given by parameter units
 
-    @param x: number of celsius
-    @param units: units that user requested
-    @return: number x converted to given units
-    """
-    if units == 'K':
-        return x + 273.15
-    elif units == 'F':
-        return (x * 9/5) + 32
+        @param x: number of kilometers
+        @param units: units that user requested
+        @return: number x converted to given units
+        """
+        if units == 'm':
+            return x * 1000
+        elif units == 'cm':
+            return x * 100000
+        elif units == 'mm':
+            return x * 1000**2
+        elif units == 'mi':
+            return x * 0.62137199
+
+
+    def convert_time(self, x, units):
+        """
+        Method converts number x to units given by parameter units
+
+        @param x: number of hours
+        @param units: units that user requested
+        @return: number x converted to given units
+        """
+        if units == 'min':
+            return x * 60
+        elif units == 's':
+            return x * 3600
+        elif units == 'day':
+            return x * 0.0416667
+
+
+    def convert_degrees(self, x, units):
+        """
+        Method converts number x to units given by parameter units
+
+        @param x: number of celsius
+        @param units: units that user requested
+        @return: number x converted to given units
+        """
+        if units == 'K':
+            return x + 273.15
+        elif units == 'F':
+            return (x * 9/5) + 32
