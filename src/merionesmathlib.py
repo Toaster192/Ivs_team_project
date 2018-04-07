1572210c60894a9654101b7c7e03678f98585b2f
Ivana Saranová <xsaran02@stud.fit.vutbr.cz>
2018-04-07 04:10:26 +0200

    Corrected another small error in root function

--- src/merionesmathlib.py
+++ src/merionesmathlib.py
@@ -149,7 +149,7 @@ class MerionesLib:
         if type(rvalue) != int or (rvalue <= 0 and rvalue % 2 == 0):
             print("Error - root value has to be a positive or even, negative integer!")
             raise ValueError('Ma ERROR')
-        if n < 0:
+        if n < 0 and rvalue % 2 != 0:
             n = -n
             return -1**(n**(1/rvalue))
         return n ** (1/rvalue)
