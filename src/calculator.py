36ea0a7a5a33c36f2f1fd89d927863aed39ff07c
Jaromír Wysoglad <xwysog00@stud.fit.vutbr.cz>
2018-04-10 16:56:05 +0200

    Rewrote big part of the file. So now buttons work properly, the result is calculated and shown on the display and keyboadr input now works

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from ui_calculator import Ui_Calculator
+from merionesmathlib import MerionesLib
 
 class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
 
@@ -10,23 +11,32 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         self.setupUi(self)
         self.show()
 
-        self.pushButton_zero.clicked.connect(self.digit_pressed)
-        self.pushButton_one.clicked.connect(self.digit_pressed)
-        self.pushButton_two.clicked.connect(self.digit_pressed)
-        self.pushButton_three.clicked.connect(self.digit_pressed)
-        self.pushButton_four.clicked.connect(self.digit_pressed)
-        self.pushButton_five.clicked.connect(self.digit_pressed)
-        self.pushButton_six.clicked.connect(self.digit_pressed)
-        self.pushButton_seven.clicked.connect(self.digit_pressed)
-        self.pushButton_eight.clicked.connect(self.digit_pressed)
-        self.pushButton_nine.clicked.connect(self.digit_pressed)
+        self.pushButton_zero.clicked.connect(self.button_pressed)
+        self.pushButton_one.clicked.connect(self.button_pressed)
+        self.pushButton_two.clicked.connect(self.button_pressed)
+        self.pushButton_three.clicked.connect(self.button_pressed)
+        self.pushButton_four.clicked.connect(self.button_pressed)
+        self.pushButton_five.clicked.connect(self.button_pressed)
+        self.pushButton_six.clicked.connect(self.button_pressed)
+        self.pushButton_seven.clicked.connect(self.button_pressed)
+        self.pushButton_eight.clicked.connect(self.button_pressed)
+        self.pushButton_nine.clicked.connect(self.button_pressed)
 
-        self.pushButton_plus.clicked.connect(self.sign_pressed)
-        self.pushButton_equal.clicked.connect(self.sign_pressed)
-        self.pushButton_dot.clicked.connect(self.sign_pressed)
+        self.pushButton_plus.clicked.connect(self.button_pressed)
+        self.pushButton_minus.clicked.connect(self.button_pressed)
+        self.pushButton_division.clicked.connect(self.button_pressed)
+        self.pushButton_factorial.clicked.connect(self.button_pressed)
+        self.pushButton_ln.clicked.connect(self.button_pressed)
+        self.pushButton_multiply.clicked.connect(self.button_pressed)
+        self.pushButton_power.clicked.connect(self.button_pressed)
+        self.pushButton_root.clicked.connect(self.button_pressed)
 
+        self.pushButton_cross.clicked.connect(self.erase)
 
-        self.pushButton_plus.clicked.connect(self.binary_operation_pressed)
+        self.pushButton_leftBracket.clicked.connect(self.button_pressed)
+        self.pushButton_rightbracket.clicked.connect(self.button_pressed)
+
+        self.pushButton_dot.clicked.connect(self.button_pressed)
 
         self.pushButton_equal.clicked.connect(self.equals_pressed)
 
@@ -35,32 +45,19 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         self.pushButton_multiply.setCheckable(True)
         self.pushButton_division.setCheckable(True)
 
-    def digit_pressed(self):
+    def button_pressed(self):
         button = self.sender()
+        expression = self.input.text() + button.text()
 
-        newInput = format(str(self.input.text() + button.text()))
+        self.input.setText(expression)
+        self.input.setFocus(False)
 
-        self.input.setText(newInput)
-
-    def sign_pressed(self):
-        button = self.sender()
-
-        newInput = self.input.text() + button.text()
-
-        self.input.setText(str(newInput))
-
-    def binary_operation_pressed(self):
-        button = self.sender()
-
-        self.firstNumber = self.input.text()
-
-        button.setChecked(True)
+    def erase(self):
+        self.input.setText("")
 
     #prepared for implementation, here is where whole equation will be solved
     def equals_pressed(self):
-        secondNumber = str(self.input.text())
-
-        if self.pushButton_plus.isChecked():
-            labelNumber = self.firstNumber + secondNumber
-            self.pushButton_plus.setChecked(False)
+        result = MerionesLib.parse_parentheses(self.input.text())
+        self.input.setText(result)
+        self.input.setFocus(False)
 
