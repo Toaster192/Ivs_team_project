d3e93153c2e85a101763470a147d4fec36f8f65a
Silvie Chlupova <sisi.chlupova@gmail.com>
2018-04-15 18:19:03 +0200

    Added new class for meriones convertor

--- src/calculator.py
+++ src/calculator.py
@@ -1,13 +1,15 @@
 from PyQt5 import QtWidgets, QtCore
 from ui_calculator import Ui_Calculator
+from ui_calculator_advanced import Ui_Advanced
 from merionesmathlib import MerionesLib
+from PyQt5.QtGui import *
 
 ##
 # @brief Class that serves as a bridge between GUI and merionesmathlib
 #
 # Reacts on button presses and either appends the needed number to the calculator display
 # or when the "equals" button is pressed, this passes the inputed string to the mathematical
-# library which computes the result of the expression and then the ClaculatorWindow shows
+# library which computes the result of the expression and then the CalculatorWindow shows
 # the result on the display
 #
 class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
@@ -19,6 +21,11 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         super().__init__()
         self.setupUi(self)
         self.show()
+
+        pixmap = QPixmap('icons8-hamster-50.png')
+        self.mouse_2.resize(50, 30)
+        self.mouse_2.move(3,0)
+        self.mouse_2.setPixmap(pixmap)
 
         self.pushButton_zero.clicked.connect(self.button_pressed)
         self.pushButton_one.clicked.connect(self.button_pressed)
@@ -53,6 +60,12 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         self.pushButton_minus.setCheckable(True)
         self.pushButton_multiply.setCheckable(True)
         self.pushButton_division.setCheckable(True)
+
+        self.pushButton_convertor.clicked.connect(self.on_button_pressed)
+
+    def on_button_pressed(self):
+        self.second = Convertor()
+        self.second.show()
 
     ##
     # @brief Appends the value of just pressed button on the display
@@ -90,3 +103,60 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
             result = str(e)
         self.input.setText(result)
         self.input.setFocus(False)
+
+##
+# @brief Class that serves as a bridge between GUI and merionesmathlib extra functions
+#
+# User input number, in dropdown choose units and result is shown in the second box
+#
+class Convertor(QtWidgets.QMainWindow, Ui_Advanced):
+
+    ##
+    # @brief Initializes the convertor window
+    #
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+        self.comboBox.addItem("kg")
+        self.comboBox.addItem("mg")
+        self.comboBox.addItem("g")
+        self.comboBox.addItem("lb")
+        self.comboBox.addItem("t")
+
+        self.comboBox_2.addItem("km")
+        self.comboBox_2.addItem("m")
+        self.comboBox_2.addItem("cm")
+        self.comboBox_2.addItem("mm")
+        self.comboBox_2.addItem("mi")
+
+        self.comboBox_3.addItem("h")
+        self.comboBox_3.addItem("min")
+        self.comboBox_3.addItem("s")
+        self.comboBox_3.addItem("day")
+
+        self.comboBox_4.addItem("C")
+        self.comboBox_4.addItem("K")
+        self.comboBox_4.addItem("F")
+
+        self.comboBox_5.addItem("kg")
+        self.comboBox_5.addItem("mg")
+        self.comboBox_5.addItem("g")
+        self.comboBox_5.addItem("lb")
+        self.comboBox_5.addItem("t")
+
+        self.comboBox_6.addItem("km")
+        self.comboBox_6.addItem("m")
+        self.comboBox_6.addItem("cm")
+        self.comboBox_6.addItem("mm")
+        self.comboBox_6.addItem("mi")
+
+        self.comboBox_7.addItem("h")
+        self.comboBox_7.addItem("min")
+        self.comboBox_7.addItem("s")
+        self.comboBox_7.addItem("day")
+
+        self.comboBox_8.addItem("C")
+        self.comboBox_8.addItem("K")
+        self.comboBox_8.addItem("F")
